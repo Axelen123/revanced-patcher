49ae4e6e45439119aa9febe2c51ae8b190e118d4
Ax333l <main@axelen.xyz>
2023-03-20 18:04:33 +0100

    actually... this is a lot better

--- src/main/kotlin/app/revanced/patcher/apk/arsc/ResourceMapper.kt
+++ src/main/kotlin/app/revanced/patcher/apk/arsc/ResourceMapper.kt
@@ -1,5 +1,6 @@
 package app.revanced.patcher.apk.arsc
 
+import app.revanced.patcher.patch.PatchResult
 import com.reandroid.arsc.chunk.PackageBlock
 import com.reandroid.arsc.chunk.TypeBlock
 
@@ -7,10 +8,6 @@ import com.reandroid.arsc.chunk.TypeBlock
  * Used for finding resource IDs.
  */
 class ResourceMapper(packageBlock: PackageBlock) {
-    class ResourceNotFoundException(val type: String, val name: String) : Exception() {
-        override fun toString() = "Could not find ID for resource \"${name}\" of type \"${type}\""
-    }
-
     internal val types = packageBlock.listAllSpecTypePair().associateBy {
         it.removeNullEntries(0)
         it.removeEmptyTypeBlocks()
@@ -22,5 +19,6 @@ class ResourceMapper(packageBlock: PackageBlock) {
     private fun TypeBlock.findId(name: String) = listEntries().find { it.name == name }?.resourceId?.toLong()
 
     fun find(type: String, name: String) =
-        types[type]?.listTypeBlocks()?.firstNotNullOfOrNull { it.findId(name) } ?: throw ResourceNotFoundException(type, name)
+        types[type]?.listTypeBlocks()?.firstNotNullOfOrNull { it.findId(name) }
+            ?: throw PatchResult.Error("Could not find resource \"${name}\" of type \"${type}\"")
 }
