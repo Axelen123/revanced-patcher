3662ede960235120e9243258e2be3fcf06681ca9
Ax333l <main@axelen.xyz>
2023-04-02 22:32:41 +0200

    no need for arsclib logger anymore

--- src/main/kotlin/app/revanced/patcher/logging/Logger.kt
+++ src/main/kotlin/app/revanced/patcher/logging/Logger.kt
@@ -1,20 +1,9 @@
 package app.revanced.patcher.logging
-
-import com.reandroid.apk.APKLogger
-
-interface Logger : APKLogger {
+interface Logger {
     fun error(msg: String) {}
     fun warn(msg: String) {}
     fun info(msg: String) {}
     fun trace(msg: String) {}
-    override fun logError(msg: String?, tr: Throwable?) {
-        error("[ARSCLIB ERROR]: $msg")
-        if (tr != null) {
-            error(tr.stackTraceToString())
-        }
-    }
-    override fun logMessage(msg: String?) = info("[ARSCLIB Info] ${msg}")
-    override fun logVerbose(msg: String?) = info("[ARSCLIB Trace] ${msg}")
 
     object Nop : Logger
 }
