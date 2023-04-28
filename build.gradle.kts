ab0dfc42939fdde0b6471d952b241396f759804a
Ax333l <main@axelen.xyz>
2023-04-28 08:35:48 +0200

    explain why an arsclib fork is needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("xpp3:xpp3:1.1.4c")
     implementation("app.revanced:smali:2.5.3-a3836654")
     implementation("app.revanced:multidexlib2:2.5.3-a3836654")
+    // ARSCLib fork with a custom zip implementation to fix performance issues on Android devices
     implementation("app.revanced:arsclib:1.1.6")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
