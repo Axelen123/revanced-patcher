5b091991c20a3638f8c67e42a1cb209cfc4c15a2
Ax333l <main@axelen.xyz>
2023-05-03 08:39:29 +0200

    more PatchBundle refactoring

--- src/main/kotlin/app/revanced/patcher/util/patch/PatchBundle.kt
+++ src/main/kotlin/app/revanced/patcher/util/patch/PatchBundle.kt
@@ -12,27 +12,22 @@ import java.util.jar.JarFile
 /**
  * A patch bundle.
  */
-sealed class PatchBundle {
-    protected companion object {
-        val parent: ClassLoader = PatchBundle::class.java.classLoader
-    }
-
+sealed class PatchBundle : Iterable<PatchClass> {
     internal abstract fun classLoader(): ClassLoader
     internal abstract val patchClassNames: Iterable<String>
 
-    /**
-     * Reads patches from the patch bundle.
-     **/
-    fun readPatches() = with(classLoader()) {
-        buildList {
-            patchClassNames.sorted().forEach { className ->
-                loadClass(className).let {
-                    if (!it.isAnnotationPresent(app.revanced.patcher.patch.annotations.Patch::class.java)) return@forEach
-                    @Suppress("UNCHECKED_CAST") this@buildList.add(it as PatchClass)
-                }
-            }
+    private val patches = sequence {
+        with(classLoader()) {
+            yieldAll(patchClassNames.map { loadClass(it) })
         }
+    }.filter {
+        it.isAnnotationPresent(app.revanced.patcher.patch.annotations.Patch::class.java)
+    }.map {
+        @Suppress("UNCHECKED_CAST")
+        it as PatchClass
     }
+
+    override fun iterator() = patches.iterator()
 
     /**
      * A patch bundle of type [Jar].
@@ -40,7 +35,7 @@ sealed class PatchBundle {
      * @param patchBundlePath The path to a patch bundle.
      */
     class Jar(private val patchBundlePath: String) : PatchBundle() {
-        override fun classLoader() = URLClassLoader(arrayOf(File(patchBundlePath).toURI().toURL()), parent)
+        override fun classLoader() = URLClassLoader(arrayOf(File(patchBundlePath).toURI().toURL()), javaClass.classLoader)
         override val patchClassNames =
             JarFile(patchBundlePath)
                 .entries()
@@ -58,7 +53,7 @@ sealed class PatchBundle {
      * @param patchBundlePath The path to a patch bundle of dex format.
      */
     class Dex(private val patchBundlePath: String) : PatchBundle() {
-        override fun classLoader() = PathClassLoader(patchBundlePath, null, parent)
+        override fun classLoader() = PathClassLoader(patchBundlePath, null, javaClass.classLoader)
         override val patchClassNames = DexFileFactory.loadDexFile(patchBundlePath, null).classes.map {
             it.type
                 .substring(1, it.length - 1)
