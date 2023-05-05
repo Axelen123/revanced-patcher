9f8dcc4a0cc54b9574dfd3ae9a4dd00f4cd461a7
Ax333l <main@axelen.xyz>
2023-05-05 09:30:12 +0200

    document LazyXMLInputSource

--- src/main/kotlin/app/revanced/patcher/util/xml/LazyXMLInputSource.kt
+++ src/main/kotlin/app/revanced/patcher/util/xml/LazyXMLInputSource.kt
@@ -5,8 +5,19 @@ import com.reandroid.apk.xmlencoder.XMLEncodeSource
 import com.reandroid.xml.XMLDocument
 import com.reandroid.xml.source.XMLDocumentSource
 
+/**
+ * Archive input source that lazily encodes the [XMLDocument] when you read from it.
+ *
+ * @param name The file name of this input source.
+ * @param document The [XMLDocument] to encode.
+ * @param materials The [EncodeMaterials] to use when encoding the document.
+ */
 internal class LazyXMLInputSource(name: String, val document: XMLDocument, materials: EncodeMaterials) : XMLEncodeSource(materials, XMLDocumentSource(name, document)) {
+    /**
+     * Encode the [XMLDocument] associated with this input source.
+     */
     fun encode() {
+        // This will call XMLEncodeSource.getResXmlBlock(), which will encode the document if it has not already been encoded.
         resXmlBlock
     }
 }
