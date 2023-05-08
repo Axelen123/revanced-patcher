3ac681996c94af8a6447eeb4002ee97e00871ace
Ax333l <main@axelen.xyz>
2023-05-08 18:40:06 +0200

    document the custom exceptions

--- src/main/kotlin/app/revanced/patcher/apk/Apk.kt
+++ src/main/kotlin/app/revanced/patcher/apk/Apk.kt
@@ -344,11 +344,20 @@ sealed class Apk private constructor(internal val module: ApkModule) {
          */
         class Encode(message: String, throwable: Throwable? = null) : ApkException(message, throwable)
 
+        /**
+         * An exception thrown when a reference could not be resolved.
+         *
+         * @param ref The invalid reference.
+         * @param throwable The corresponding [Throwable].
+         */
         class ReferenceError(ref: String, throwable: Throwable? = null) :
             ApkException("Failed to resolve: $ref", throwable) {
             constructor(type: String, name: String, throwable: Throwable? = null) : this("@$type/$name", throwable)
         }
 
+        /**
+         * An exception thrown when the [Apk] does not have a resource table, but was expected to have one.
+         */
         object MissingResourceTable : ApkException("Apk does not have a resource table.")
     }
 }
--- src/main/kotlin/app/revanced/patcher/patch/PatchException.kt
+++ src/main/kotlin/app/revanced/patcher/patch/PatchException.kt
@@ -1,6 +1,12 @@
 package app.revanced.patcher.patch
 
-class PatchException constructor(errorMessage: String?, cause: Throwable?) : Exception(errorMessage, cause) {
+/**
+ * An exception thrown when patching.
+ *
+ * @param errorMessage The exception message.
+ * @param cause The corresponding [Throwable].
+ */
+class PatchException(errorMessage: String?, cause: Throwable?) : Exception(errorMessage, cause) {
     constructor(errorMessage: String) : this(errorMessage, null)
     constructor(cause: Throwable) : this(cause.message, cause)
 }
